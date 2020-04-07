965dbd94865eb5944889d8607f5d0060a3958e9d
Can Aybalık <can.moonfish@gmail.com>
2020-04-07 09:27:20 +0300

    can-dev: branch commit

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         System.out.println("Hello World");
         System.out.println("Merhaba Dünya!");
         System.out.println("can-dev");
+        System.out.println("can-dev");
 
     }
 }
