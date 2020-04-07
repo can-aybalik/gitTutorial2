7c607fa0a7ae6463e707be38c44e0360e69762db
Can Aybalık <can.moonfish@gmail.com>
2020-04-07 09:31:41 +0300

    master: continue

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ public class Main {
         System.out.println("can-dev");
         System.out.println("can-dev");
         System.out.println("can-dev");
+        System.out.println("can-dev");
 
     }
 }
